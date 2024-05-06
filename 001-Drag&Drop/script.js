9d7a22a8caa75d4bc752963bf61f7202cdfd238a
ArthurJuwer <arthur.juwer99@gmail.com>
2024-05-05 22:02:33 -0300

    Drag and Drop

--- 001-Drag&Drop/script.js
+++ 001-Drag&Drop/script.js
@@ -3,17 +3,28 @@
 const boxes = document.querySelectorAll(".box"),
  image = document.querySelector(".image")
 
-boxes.forEach((box) => {
+boxes.forEach((box) => {    
+
+    if(box.contains(image) == false){
+        box.innerHTML = '<p>DROP</p>'
+    }
     box.addEventListener("dragover", (e) =>{
         e.preventDefault()
+        box.innerHTML = ''
         box.classList.add("hovered")
+       
+        
     });
 
     box.addEventListener("dragleave", ()=>{
+        box.innerHTML = '<p>DROP</p>'
         box.classList.remove("hovered")
+        
+        
     })
 
     box.addEventListener("drop", ()=>{
         box.appendChild(image)
     })
+    
 });
