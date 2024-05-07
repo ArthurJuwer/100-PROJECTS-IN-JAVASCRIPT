cb5b9e8fb8fcbd8288757ac6987b4f6c380f3751
ArthurJuwer <arthur.juwer99@gmail.com>
2024-05-07 12:26:51 -0300

    Timer created for the pomodoro method

---
+++ 04-PomodoroTimer/script.js
@@ -0,0 +1,48 @@
+let atualTime = document.querySelector(".timer input")
+
+let interval;
+let timeLeft = 62
+
+updateTimer()
+
+
+function updateTimer(){
+
+    let minutes = Math.floor(timeLeft / 60)
+    let seconds = timeLeft % 60
+    let formattedTime = `${minutes.toString().padStart(2, "0")} : ${seconds.toString().padStart(2,"0")}`
+
+    atualTime.value = formattedTime
+
+}
+
+function startTimer(){
+    interval = setInterval(() => {
+            timeLeft--;
+            updateTimer()
+            if(timeLeft === 0){
+                clearInterval(interval)
+                alert("Times's Up!")
+            }
+        }, 1000);
+}
+
+function stopTimer(){
+    clearInterval(interval)
+    updateTimer()
+}
+function resetTimer(){
+    clearInterval(interval)
+    timeLeft = 1500
+    updateTimer()
+}
+
+
+const btnStart = document.querySelector("#start")
+const btnStop = document.querySelector("#stop")
+const btnReset = document.querySelector("#reset")
+
+
+btnStart.addEventListener("click", startTimer)
+btnStop.addEventListener("click", stopTimer)
+btnReset.addEventListener("click", resetTimer)
