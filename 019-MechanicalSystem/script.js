630e26226185c695b969756e73f5143980c96e88
ArthurJuwer <arthur.juwer99@gmail.com>
2024-05-21 20:40:58 -0300

    One more step completed, now all that's left is the input search function

--- 019-MechanicalSystem/script.js
+++ 019-MechanicalSystem/script.js
@@ -1,19 +1,27 @@
 const sectionResult = document.querySelector(".result-stage")
+const formAdd = document.querySelector("#form-add")
+const plateInput = document.querySelector("#plate")
+const brandInput = document.querySelector("#brand")
+const yearInput = document.querySelector("#year")
+const dateEntryInput = document.querySelector("#dateEntry")
+const problemInput = document.querySelector("#problem")
 
-let itens = []
+let items = []
 
-function renderInfo(){
-
-
+function renderInfo() {
     sectionResult.innerHTML = '';
-
-
-    itens.forEach((item, index) => {
+    items.forEach((item, index) => {
         const ul = document.createElement("ul")
         ul.setAttribute("class", "result-content")
         ul.innerHTML = `
         <li class="image">
-        <span></span>
+            <div class="image-input">
+                <input type="file" accept="image/*" id="imageInput-${index}" onchange="newImage(${index})">
+                <label for="imageInput-${index}" class="image-button" id="imageLabel-${index}" style="display: ${item.imageSrc ? 'none' : 'flex'};">
+                    <img src="/019-MechanicalSystem/images/upload.png" alt="Upload">
+                </label>
+                <img src="${item.imageSrc}" class="image-preview change-image" id="imagePreview-${index}" onclick="changeImage(${index})" style="display: ${item.imageSrc ? 'block' : 'none'};">
+            </div>
         </li>
         <li>${item.plate}</li>
         <li>${item.brand}</li>
@@ -21,74 +29,81 @@ function renderInfo(){
         <li>${item.dateEntry}</li>
         <li>${item.problem}</li>
         <li class="actions">
-                <button onclick="toggleStatus(${index})"> 
-                    <span style="background-color: ${item.colorStats};"></span>
-                    <div class="balao-mensagem">
-                        <p>${item.stats}</p>
-                    </div>
-                </button>
-                <button onclick="removeItems(${index})"> 
-                    <img src="/019-MechanicalSystem/images/trash.png" alt="">
-                </button>
-            </li>
+            <button onclick="toggleStatus(${index})"> 
+                <span style="background-color: ${item.statsColor};"></span>
+                <div class="balao-mensagem">
+                    <p>${item.statsChecked}</p>
+                </div>
+            </button>
+            <button onclick="removeItems(${index})"> 
+                <img src="/019-MechanicalSystem/images/trash.png" alt="Delete">
+            </button>
+        </li>
         `
         sectionResult.appendChild(ul)
     });
-
-    
 }
 
-function addItems(plate, brand, year, dateEntry, problem, stats, colorStats){
-
-    const item = {plate, brand, year, dateEntry, problem, stats, colorStats}
-    itens.push(item)
+function addItems(plate, brand, year, dateEntry, problem, statsChecked, statsColor, imageSrc = '') {
+    const item = { plate, brand, year, dateEntry, problem, statsChecked, statsColor, imageSrc }
+    items.push(item)
     renderInfo()
 }
 
-function removeItems(index){
-    itens.splice(index, 1)
-    renderInfo()    
+function removeItems(index) {
+    items.splice(index, 1)
+    renderInfo()
 }
-function toggleStatus(index){    
 
-    if(itens[index].stats == 'Pending'){
-        itens[index].stats = 'Approved'
-        itens[index].colorStats = 'green'
-        
+function toggleStatus(index) {
+    items[index].statsChecked = (items[index].statsChecked === 'Pending') ? 'Approved' : 'Pending';
+    items[index].statsColor = (items[index].statsChecked === 'Approved') ? 'green' : '#FFBF00';
+    renderInfo()
+}
+
+formAdd.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let plate = plateInput.value.trim()
+    let brand = brandInput.value.trim()
+    let year = parseInt(yearInput.value)
+    let dateEntry = dateEntryInput.value
+    let problem = problemInput.value.trim()
+    let statsChecked = document.querySelector('input[name="quest"]:checked').value
+    let statsColor = statsChecked === 'Approved' ? 'green' : '#FFBF00'
+    
+    if (!isNaN(year) && year >= 1886) {
+        addItems(plate, brand, year, dateEntry, problem, statsChecked, statsColor)
     } else {
-        itens[index].stats = 'Pending'
-        itens[index].colorStats = '#FFBF00'
+        alert('Invalid values')
     }
-    renderInfo()
-}
-const btnRegister = document.querySelector('#buttonRegister')
-
-btnRegister.addEventListener("click", ()=>{
-    let plateValue = document.querySelector("#plate")
-    let brandValue = document.querySelector("#brand")
-    let yearValue = document.querySelector("#year")
-    let dateEntryValue = document.querySelector("#dateEntry")
-    let problemValue = document.querySelector("#problem")
-    let radios = document.querySelectorAll("[id='quest']");
-    let selectedValue = 'Pending';
-    let colorStats = '#FFBF00'
-
-    if(plateValue.value, brandValue.value, yearValue.value, dateEntryValue.value, problemValue.value == ''){
-        alert("insert the values bellow:")
-        return
-    } 
-
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            selectedValue = radios[i].nextElementSibling.textContent.trim();
-        }
-    }
-    
-    if(selectedValue == 'Approved'){
-        colorStats = 'green'
-    }
-
-    addItems(plateValue.value,brandValue.value, yearValue.value, dateEntryValue.value, problemValue.value, selectedValue, colorStats)
 })
 
-renderInfo()
+function newImage(index) {
+    let input = document.getElementById(`imageInput-${index}`);
+    let imagePreview = document.getElementById(`imagePreview-${index}`);
+    let label = document.getElementById(`imageLabel-${index}`);
+
+    if (input.files && input.files[0]) {
+        let fileReader = new FileReader()
+        fileReader.onload = function (event) {
+            imagePreview.src = event.target.result
+            items[index].imageSrc = event.target.result
+            label.style.display = 'none'
+            imagePreview.style.display = 'block'
+        }
+        fileReader.readAsDataURL(input.files[0]);
+    }
+}
+
+function changeImage(index) {
+    let input = document.getElementById(`imageInput-${index}`)
+    let label = document.getElementById(`imageLabel-${index}`)
+    let imagePreview = document.getElementById(`imagePreview-${index}`)
+    
+    input.value = ''
+    items[index].imageSrc = ''
+    label.style.display = 'flex' 
+    imagePreview.style.display = 'none'
+}
+
+renderInfo();
